9e227cd4f17af08de2e85ca362d50dc9b7e48d2e
Vitalx <liamdu92@gmail.com>
2024-12-04 11:44:20 +0100

    feat(init-back): change how starting the app in main.ts

--- back-app/src/main.ts
+++ back-app/src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import "./env";
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(ENV.PORT);
-}
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+void NestFactory.create(AppModule)
+	.then((app) => {
+		void app.listen(ENV.PORT);
+	});
