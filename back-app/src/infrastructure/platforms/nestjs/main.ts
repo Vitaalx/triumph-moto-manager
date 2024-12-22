0af8fae9fd6786eac85aaac29e661a4b5343dbaf
Liam MACQUAIRE <liamdu92@gmail.com>
2024-12-22 18:14:29 +0100

    feat(init-back): add prisma conf

--- back-app/src/infrastructure/platforms/nestjs/main.ts
+++ back-app/src/infrastructure/platforms/nestjs/main.ts
@@ -5,4 +5,13 @@ import "@config/env";
 void NestFactory.create(AppModule)
 	.then((app) => {
 		void app.listen(ENV.PORT);
+		process.stdout.write(
+			`====================================\n\n` +
+			`Server has started correctly on:\n\n` +
+			 `- Port: ${ENV.PORT}\n` +
+			 `- Host: ${ENV.HOST}\n` +
+			 `- Environment: ${ENV.ENVIRONMENT}\n` +
+			 `- Origin: ${ENV.ORIGIN}\n\n` +
+			`====================================\n`,
+		);
 	});
