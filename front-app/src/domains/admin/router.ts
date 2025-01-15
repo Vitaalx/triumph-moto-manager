8842a45d63f5c18ae03c41e6f36c34bae4636c1b
EU\U017636 <maubry@scor.com>
2025-01-15 14:33:51 +0100

    feat: update AdminNavbar

--- front-app/src/domains/admin/router.ts
+++ front-app/src/domains/admin/router.ts
@@ -1,18 +1,42 @@
 import type { RouteRecordRaw } from "vue-router";
 
 export const routerPageNameAdminPanel = Object.freeze({
-	ADMIN_PANEL_HOME: "admin-panel",
+	DASHBOARD: "dashboard",
+	MOTORBIKE_LIST: "motorbike-list",
+	MOTORBIKE_ADD: "motorbike-add",
+	MAINTENANCE_PLANNING: "maintenance-planning",
+	MAINTENANCE_HISTORY: "maintenance-history",
 });
 
 export default (): RouteRecordRaw[] => [
 	{
-		path: "/admin-panel",
+		path: "/dashboard",
 		component: () => import("./layout/AdminLayout.vue"),
 		children: [
 			{
-				name: routerPageNameAdminPanel.ADMIN_PANEL_HOME,
-				path: "/admin-panel",
-				component: () => import("./pages/HomeAdminPanel.vue"),
+				name: routerPageNameAdminPanel.DASHBOARD,
+				path: "/dashboard",
+				component: () => import("./pages/AdminHomePage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.MOTORBIKE_LIST,
+				path: "/dashboard/motorbike-list",
+				component: () => import("./pages/MotorbikeListPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.MOTORBIKE_ADD,
+				path: "/dashboard/motorbike-add",
+				component: () => import("./pages/MotorbikeAddPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.MAINTENANCE_PLANNING,
+				path: "/dashboard/maintenance-planning",
+				component: () => import("./pages/MaintenancePlanningPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.MAINTENANCE_HISTORY,
+				path: "/dashboard/maintenance-history",
+				component: () => import("./pages/MaintenanceHistoryPage.vue"),
 			},
 		],
 	},
