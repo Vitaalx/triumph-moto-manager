0fe1a79d364ef6085cc9d74a9dddd607a7ea3097
EU\U017636 <maubry@scor.com>
2025-01-15 16:33:25 +0100

    feat: update dashboard

--- front-app/src/domains/admin/router.ts
+++ front-app/src/domains/admin/router.ts
@@ -6,6 +6,20 @@ export const routerPageNameAdminPanel = Object.freeze({
 	MOTORBIKE_ADD: "motorbike-add",
 	MAINTENANCE_PLANNING: "maintenance-planning",
 	MAINTENANCE_HISTORY: "maintenance-history",
+	PIECE_LIST: "piece-list",
+	PIECE_ADD: "piece-add",
+	PIECE_DELIVERY_HISTORY: "piece-delivery-history",
+	PIECE_SUPPLIER_LIST: "piece-supplier-list",
+	PIECE_SUPPLIER_ADD: "piece-supplier-add",
+	USER_LIST: "user-list",
+	USER_ADD: "user-add",
+	PERMISSION_MANAGEMENT: "permission-management",
+	DRIVER_LIST: "driver-list",
+	DRIVER_ADD: "driver-add",
+	INCIDENT_HISTORY: "incident-history",
+	TRY_MOTORBIKE_LIST: "try-motorbike-list",
+	TRY_HISTORY: "try-history",
+	TROUBLESHOOTING_ADD: "troubleshooting-add",
 });
 
 export default (): RouteRecordRaw[] => [
@@ -38,6 +52,76 @@ export default (): RouteRecordRaw[] => [
 				path: "/dashboard/maintenance-history",
 				component: () => import("./pages/MaintenanceHistoryPage.vue"),
 			},
+			{
+				name: routerPageNameAdminPanel.PIECE_LIST,
+				path: "/dashboard/piece-list",
+				component: () => import("./pages/PieceListPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.PIECE_ADD,
+				path: "/dashboard/piece-add",
+				component: () => import("./pages/PieceAddPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.PIECE_DELIVERY_HISTORY,
+				path: "/dashboard/piece-delivery-history",
+				component: () => import("./pages/PieceDeliveryHistoryPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.PIECE_SUPPLIER_LIST,
+				path: "/dashboard/piece-supplier-list",
+				component: () => import("./pages/PieceSupplierListPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.PIECE_SUPPLIER_ADD,
+				path: "/dashboard/piece-supplier-add",
+				component: () => import("./pages/PieceSupplierAddPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.USER_LIST,
+				path: "/dashboard/user-list",
+				component: () => import("./pages/UserListPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.USER_ADD,
+				path: "/dashboard/user-add",
+				component: () => import("./pages/UserAddPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.PERMISSION_MANAGEMENT,
+				path: "/dashboard/permission-management",
+				component: () => import("./pages/PermissionManagementPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.DRIVER_LIST,
+				path: "/dashboard/driver-list",
+				component: () => import("./pages/DriverListPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.DRIVER_ADD,
+				path: "/dashboard/driver-add",
+				component: () => import("./pages/DriverAddPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.INCIDENT_HISTORY,
+				path: "/dashboard/incident-history",
+				component: () => import("./pages/IncidentHistoryPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.TRY_MOTORBIKE_LIST,
+				path: "/dashboard/try-motorbike-list",
+				component: () => import("./pages/TryMotorbikeListPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.TRY_HISTORY,
+				path: "/dashboard/try-history",
+				component: () => import("./pages/TryHistoryPage.vue"),
+			},
+			{
+				name: routerPageNameAdminPanel.TROUBLESHOOTING_ADD,
+				path: "/dashboard/troubleshooting-add",
+				component: () => import("./pages/TroubleshootingAddPage.vue"),
+			}
 		],
 	},
 ];
