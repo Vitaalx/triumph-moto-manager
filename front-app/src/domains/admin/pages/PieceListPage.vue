3b57ea81c1a96329308c3baf8d97115d13616cc9
Maubry94 <matthieu.aubry94370@gmail.com>
2025-01-19 16:00:43 +0100

    feat: add pieces list

--- front-app/src/domains/admin/pages/PieceListPage.vue
+++ front-app/src/domains/admin/pages/PieceListPage.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import TheButton from "@/components/ui/button/TheButton.vue";
 import AdminSection from "../components/AdminSection.vue";
-import SearchInput from "@/components/SearchInput.vue";
+import PieceTable from "../components/PieceTable.vue";
 </script>
 
 <template>
 	<AdminSection title="Liste des pièces">
-		<div class="flex justify-between items-center">
-			<SearchInput />
-
-			<TheButton>Ajouter une pièce</TheButton>
-		</div>
+		<PieceTable />
 	</AdminSection>
 </template>
