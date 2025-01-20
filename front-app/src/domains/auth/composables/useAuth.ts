e81083edeacb3fea31e614ebecdeb1f6ed1a9c91
Maubry94 <matthieu.aubry94370@gmail.com>
2025-01-20 18:23:44 +0100

    feat: add login toast

--- front-app/src/domains/auth/composables/useAuth.ts
+++ front-app/src/domains/auth/composables/useAuth.ts
@@ -2,27 +2,45 @@ import api from "@/lib/axios";
 import { useRouter } from "vue-router";
 import { useUserStore } from "@/stores/userStore";
 import { toast } from "@/components/ui/toast";
-import { h } from "vue";
 
 export function useAuth() {
 	const router = useRouter();
 	const userStore = useUserStore();
 
-	async function login(values: { email: string; password: string }) {
+	async function login(formData: { email: string; password: string }) {
 		try {
-			const response = await api.post("/api/auth/login", values);
+			const response = await api.post("/api/auth/login", formData);
 			const userData = response.data;
 
 			userStore.user = userData;
 
-			await router.push("/");
-		} catch (error) {
 			toast({
-				title: "Identifiants incorrects",
-				description: h("div", { innerHTML: "Veuillez vérifier vos identifiants" }),
-				variant: "destructive"
+				title: "Connexion réussie !",
+				description: "Vous êtes maintenant connecté.",
+				variant: "success",
 			});
-			throw error;
+
+			router.push("/");
+		} catch (error) {
+			if (error.response.data.message === "user.notfound") {
+				toast({
+					title: "Utilisateur introuvable",
+					description: "Veuillez vérifier votre adresse email.",
+					variant: "destructive",
+				});
+			} else if (error.response.data.message === "password.invalid") {
+				toast({
+					title: "Mot de passe incorrect",
+					description: "Veuillez vérifier votre mot de passe.",
+					variant: "destructive",
+				});
+			} else {
+				toast({
+					title: "Erreur inconnue",
+					description: "Une erreur inattendue est survenue.",
+					variant: "destructive",
+				});
+			}
 		}
 	}
 
--- front-app/tailwind.config.js
+++ front-app/tailwind.config.js
@@ -86,6 +86,10 @@ module.exports = {
                     DEFAULT: "hsl(var(--destructive))",
                     foreground: "hsl(var(--destructive-foreground))",
                 },
+				success: {
+					DEFAULT: "hsl(var(--success))",
+					foreground: "hsl(var(--success-foreground))",
+				},
                 muted: {
                     DEFAULT: "hsl(var(--muted))",
                     foreground: "hsl(var(--muted-foreground))",
