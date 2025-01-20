10b516a7df1c3bc2c3eac2f15a795db9f225a2ad
Maubry94 <matthieu.aubry94370@gmail.com>
2025-01-20 12:45:18 +0100

    feat: add motorcycle add form

--- front-app/src/App.vue
+++ front-app/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import TheLoader from "@/components/TheLoader.vue";
+import TheToaster from "./components/ui/toast/TheToaster.vue";
 </script>
 
 <template>
+	<TheToaster />
+
 	<RouterView />
 
 	<TheLoader />
