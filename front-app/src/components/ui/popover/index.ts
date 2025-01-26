bef132326a1a1f8ae2f19616eb2f2dcd9e6ddd9c
Maubry94 <matthieu.aubry94370@gmail.com>
2025-01-26 22:51:15 +0100

    feat: init branch

---
+++ front-app/src/components/ui/popover/index.ts
@@ -0,0 +1,3 @@
+export { default as ThePopover } from "./ThePopover.vue";
+export { default as PopoverContent } from "./PopoverContent.vue";
+export { default as PopoverTrigger } from "./PopoverTrigger.vue";
