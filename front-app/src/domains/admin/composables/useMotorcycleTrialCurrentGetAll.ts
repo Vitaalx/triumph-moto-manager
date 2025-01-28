5c32a0d6b0f29e3dd01e399f25ee80c729f3113f
EU\U017636 <maubry@scor.com>
2025-01-28 15:49:09 +0100

    feat: clean

--- front-app/src/domains/admin/composables/useMotorcycleTrialCurrentGetAll.ts
+++ front-app/src/domains/admin/composables/useMotorcycleTrialCurrentGetAll.ts
@@ -6,16 +6,6 @@ export function useMotorcycleTrialCurrentGetAll() {
 	const motorcycleTrials = ref<formattedMotorcycleTrial[]>([]);
 	const isLoading = ref(true);
 
-	const fakeData = ref<formattedMotorcycleTrial[]>([ // TODO: remove this when works whith real data
-		{
-			id: "trial-id",
-			driverId: "driver-id",
-			motorcycleId: "motorcycle-id",
-			startDate: "2025-02-01",
-			endDate: "2025-02-02"
-		}
-	]);
-
 	function getAllMotorcycleTrialCurrent() {
 		api.get("/motorcycles-in-trial")
 			.then((response) => {
@@ -33,7 +23,6 @@ export function useMotorcycleTrialCurrentGetAll() {
 				console.error(error);
 			})
 			.finally(() => {
-				motorcycleTrials.value = fakeData.value; // TODO: try with real data
 				isLoading.value = false;
 			});
 	}
